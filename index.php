fd589723ed5b9282fad0f5c5a9851c754fc3a4ba
Jean <jdestoppani@gmail.com>
2020-12-14 10:46:02 +0100

    update: Let's Go

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
+<<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <title>Document</title>
+</head>
+<body>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, maxime.</p>
+
+<script src="assets/js/app.js"></script>
+</body>
+</html>
+
 <?php
+/*
 
 require_once 'vendor/autoload.php'; //charger dépendnaces
 
 use Carbon\Carbon;
 
 //var_dump(Carbon::now());
+
+*/
 ?>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, maxime.</p>
