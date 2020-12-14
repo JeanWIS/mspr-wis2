08d9b0bb815d95731ec3ead56bb4a18554f8c9d9
Killian LE MOAL <killian.lemoal2001@gmail.com>
2020-12-14 09:33:01 +0100

    update : debuggage

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
+
 require_once 'vendor/autoload.php'; //charger dépendnaces
 
 use Carbon\Carbon;
 
-var_dump(Carbon::now());
-
+//var_dump(Carbon::now());
 ?>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, maxime.</p>
