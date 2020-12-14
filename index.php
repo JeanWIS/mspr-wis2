3fd771262951cf3de22bde970c06cac7cea8959b
Jean <jdestoppani@gmail.com>
2020-12-14 13:03:04 +0100

    update: MondayMorning

--- index.php
+++ index.php
@@ -11,29 +11,27 @@
 <body>
 
 <header>
-
-
-
-    <nav id="navbar" class="navbar navbar-expand-lg">
-        <img id="logo" src="Logo-Ollakino.png" alt="Logo du Réseau social">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container">
+            <div class="col-2">
+                <img class="img-fluid" style="height: 100px" src="assets/images/Logo-Ollakino.png"
+                                    alt="Logo du Réseau social"></div>
+            <div class="col-6">
                 <form class="d-flex">
-                    <input id="search" class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
+                    <input id="search" class="form-control " type="search" placeholder="Search" aria-label="Search">
                     <button id="button" class="btn btn-outline-success" type="submit">Rechercher</button>
                 </form>
-            </ul>
+            </div>
+            <div class="col-4">
+            </div>
         </div>
     </nav>
 </header>
 
+
 <div class="container-fluid">
     <div class="row">
-        <nav id="menu" class="col-lg-2 col-md-4 col-sm-5 d-none d-md-block">
+        <nav id="menu" class="col-2 d-md-block">
             <div class="sidebar">
                 <i class="fas fa-home"></i><h4><a href="boostratp.html">Accueil</h4><br>
                 <h4><a href="#">Mon Profil</h4><br>
@@ -43,23 +41,29 @@
                     </a>
                     <a href="#" class="list-group-item list-group-item-action">Lien</a>
                     <a href="#" class="list-group-item list-group-item-action">Lien</a>
-                    <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1"
+                       aria-disabled="true">Disabled</a>
                 </div>
             </div>
-
         </nav>
         <div class="col-lg-9 col-md-8 col-sm-7">
             <div class="article">
                 <h1>Mon titre</h1>
                 <h6>Sous-titre, 9 septembre 2020</h6>
                 <img src="exo.jpg" alt="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus deserunt dolor, eius ipsam iusto nihil
-                    numquam possimus quod reiciendis similique. Asperiores autem culpa dolores nemo nostrum quaerat suscipit
-                    voluptatum! Accusamus ad aliquam animi blanditiis, cum cumque cupiditate debitis deleniti deserunt dolorum
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus deserunt dolor, eius ipsam iusto
+                    nihil
+                    numquam possimus quod reiciendis similique. Asperiores autem culpa dolores nemo nostrum quaerat
+                    suscipit
+                    voluptatum! Accusamus ad aliquam animi blanditiis, cum cumque cupiditate debitis deleniti deserunt
+                    dolorum
                     eligendi error facilis itaque iure modi molestias nam officiis pariatur quibusdam ratione reiciendis
-                    reprehenderit repudiandae saepe sit soluta totam voluptatem voluptatibus? Architecto, aspernatur commodi
-                    consequatur debitis dicta dignissimos ea eaque eius facilis in nobis possimus quasi reprehenderit saepe sunt
-                    ullam vero voluptatum. Aperiam commodi delectus dignissimos dolorum necessitatibus omnis placeat, porro,
+                    reprehenderit repudiandae saepe sit soluta totam voluptatem voluptatibus? Architecto, aspernatur
+                    commodi
+                    consequatur debitis dicta dignissimos ea eaque eius facilis in nobis possimus quasi reprehenderit
+                    saepe sunt
+                    ullam vero voluptatum. Aperiam commodi delectus dignissimos dolorum necessitatibus omnis placeat,
+                    porro,
                     possimus quia ratione voluptatem voluptatibus. At, quam voluptate!</p>
                 <hr/>
             </div>
@@ -69,13 +73,19 @@
                 <h1>Mon titre</h1>
                 <h6>Sous-titre, 9 septembre 2020</h6>
                 <img src="exo.jpg" alt="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus deserunt dolor, eius ipsam iusto nihil
-                    numquam possimus quod reiciendis similique. Asperiores autem culpa dolores nemo nostrum quaerat suscipit
-                    voluptatum! Accusamus ad aliquam animi blanditiis, cum cumque cupiditate debitis deleniti deserunt dolorum
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus deserunt dolor, eius ipsam iusto
+                    nihil
+                    numquam possimus quod reiciendis similique. Asperiores autem culpa dolores nemo nostrum quaerat
+                    suscipit
+                    voluptatum! Accusamus ad aliquam animi blanditiis, cum cumque cupiditate debitis deleniti deserunt
+                    dolorum
                     eligendi error facilis itaque iure modi molestias nam officiis pariatur quibusdam ratione reiciendis
-                    reprehenderit repudiandae saepe sit soluta totam voluptatem voluptatibus? Architecto, aspernatur commodi
-                    consequatur debitis dicta dignissimos ea eaque eius facilis in nobis possimus quasi reprehenderit saepe sunt
-                    ullam vero voluptatum. Aperiam commodi delectus dignissimos dolorum necessitatibus omnis placeat, porro,
+                    reprehenderit repudiandae saepe sit soluta totam voluptatem voluptatibus? Architecto, aspernatur
+                    commodi
+                    consequatur debitis dicta dignissimos ea eaque eius facilis in nobis possimus quasi reprehenderit
+                    saepe sunt
+                    ullam vero voluptatum. Aperiam commodi delectus dignissimos dolorum necessitatibus omnis placeat,
+                    porro,
                     possimus quia ratione voluptatem voluptatibus. At, quam voluptate!</p>
             </div>
 
