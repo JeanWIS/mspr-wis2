ee845ca5ef2f3b275277676cfd63fa2bc84b2add
Killian LE MOAL <killian.lemoal2001@gmail.com>
2020-12-15 17:01:43 +0100

    update: home

--- index.php
+++ index.php
@@ -25,6 +25,56 @@
 </header>
 <body>
 
+<div class="fond">
+    <div class="container">
 
+        <div class="row" style="min-height: calc(100vh - 300px)">
+
+            <div class="col-md-6 align-self-center">
+                <div class="card card-body">
+                    <form id="form" action="update.php" method="POST">
+
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="d-inline-block mb-0 p-3 bg-light text-center">Mon titre</h1>
+                                <div class="mb-3">
+                                    <label>Prénom</label>
+                                    <input class="form-control" name="first_name" id="first_name"
+                                           required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="mb-3">
+                                    <label>Nom</label>
+                                    <input class="form-control" name="last_name" id="last_name"
+                                           required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label>Date de naissance</label>
+                            <input type="date" class="form-control" name="birthday" id="birthday"
+                                   required>
+                        </div>
+
+                        <button id="form-submit" type="submit" class="btn btn-primary w-100">
+                            Partir à l'aventure !
+                        </button>
+
+                    </form>
+                </div>
+
+            </div>
+            <div class="col-md-6 align-self-center">
+                <div class="mockup">
+                    <img src="https://placehold.it/10*30">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<?php include_once '../../includes/footer.php'; ?>
 </body>
 </html>
