867fad52b1e2df7e18cc721e2f77022247288980
Jean <jdestoppani@gmail.com>
2020-12-15 14:41:40 +0100

    update: variable user

--- pages/account/delete-action.php
+++ pages/account/delete-action.php
@@ -6,5 +6,7 @@ $dbh = connectDB();
 $stmt = $dbh->prepare('DELETE FROM users WHERE users.id = :id');
 $stmt->bindValue(':id', $id);
 $stmt->execute();
+session_unset();
+
 
 header("Location: /mspr-wis2/pages/feed.php");
