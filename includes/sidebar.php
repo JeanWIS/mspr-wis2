ea974b31ef19905ddd853a5c922c96f4b3311210
Killian LE MOAL <killian.lemoal2001@gmail.com>
2020-12-15 16:29:41 +0100

    update: profile

---
+++ includes/sidebar.php
@@ -0,0 +1,8 @@
+<nav id="menu" class="col-2 bg-secondary">
+    <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active" aria-current="true"> Feed </a>
+        <a href="#" class="list-group-item list-group-item-action">My friends</a>
+        <a href="./account/profile.php" class="list-group-item list-group-item-action">Profile</a>
+        <a href="./post/add.php" class="list-group-item list-group-item-action">Add a new recipe</a>
+    </div>
+</nav>
--- pages/feed.php
+++ pages/feed.php
@@ -3,14 +3,7 @@
 
 <div class="container-fluid">
     <div class="row">
-        <nav id="menu" class="col-2 bg-secondary">
-                <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true"> Feed </a>
-                    <a href="#" class="list-group-item list-group-item-action">My friends</a>
-                    <a href="./account/profile.php" class="list-group-item list-group-item-action">Profile</a>
-                    <a href="./post/add.php" class="list-group-item list-group-item-action">Add a new recipe</a>
-                </div>
-        </nav>
+        <?php include_once '../includes/sidebar.php'; ?>
         <div class="col-lg-9 col-md-8 col-sm-7">
             <div class="article row">
                 <h1>Mon titre</h1>
