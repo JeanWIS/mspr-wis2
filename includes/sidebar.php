74f1bff51e029e859bb58504601ce36262ddefde
Killian LE MOAL <killian.lemoal2001@gmail.com>
2020-12-16 10:36:34 +0100

    update: sidebar & files

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,17 +3,9 @@
 <div class="sidebar border-right" id="sidebar-wrapper">
     <div class="sidebar-heading" style="text-align: center;"><strong>Ollakino</strong></div>
     <div class="list-group list-group-flush">
-<<<<<<< Updated upstream
-        <a href="/mspr-wis2/pages/feed.php" class="list-group-item list-group-item-action bg-light">Feed</a>
-        <a href="/mspr-wis2/pages/account/profile.php" class="list-group-item list-group-item-action">Profile</a>
-        <a href="/mspr-wis2/pages/post/add.php" class="list-group-item list-group-item-action bg-light">Add a new recipe</a>
-        <a href="/mspr-wis2/pages/post/my-posts.php" class="list-group-item list-group-item-action bg-light">See all my recipes</a>
-        <a href="/mspr-wis2/pages/user/friends.php" class="list-group-item list-group-item-action bg-light">All My friends</a>
-=======
         <a href="/mspr-wis2/pages/feed.php" class="list-group-item list-group-item-action" style="color: white; background-color: #630887;"><i class="fas fa-rss-square"></i>Fil d'actualité</a>
         <a href="/mspr-wis2/pages/account/profile.php" class="list-group-item list-group-item-action" style="color: white; background-color: #630887;"><i class="fas fa-user"></i>Profil</a>
         <a href="/mspr-wis2/pages/post/add.php" class="list-group-item list-group-item-action" style="color: white; background-color: #630887;"><i class="fas fa-plus-square"></i>Ajouter une nouvelle recette</a>
         <a href="/mspr-wis2/pages/post/my-posts.php" class="list-group-item list-group-item-action" style="color: white; background-color: #630887;"><i class="fas fa-eye"></i>Voir toutes mes recettes</a>
->>>>>>> Stashed changes
     </div>
 </div>
