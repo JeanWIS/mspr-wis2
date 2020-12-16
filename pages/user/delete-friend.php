e6631d6efb575466a57e7b0b44cb3e749b3bbd6b
Jean <jdestoppani@gmail.com>
2020-12-16 12:55:23 +0100

    update: Let's eattttttttt

---
+++ pages/user/delete-friend.php
@@ -0,0 +1,24 @@
+<?php include_once '../../includes/head.php'; ?>
+
+<?php
+$friend_id = $_GET['id']; // get the id in the URL of the random user
+$user = getUser($friend_id); // Execute the function that will find the user with the id from the URL
+
+$id = $_SESSION['user']['id'];
+
+$values = [
+    'user_id' => $id,
+    'friend_id' => $friend_id
+];
+
+
+$dbh = connectDB();
+$stmt = $dbh->prepare('DELETE FROM friends WHERE user_id = :user_id && friend_id = :friend_id');
+$stmt->bindValue(':user_id', $id);
+$stmt->bindValue(':friend_id', $friend_id);
+$stmt->execute();
+
+header("Location: http://localhost:8888/mspr-wis2/pages/user/user.php?id=$friend_id");
+
+?>
+<?php include_once '../../includes/footer.php'; ?>
--- pages/user/user.php
+++ pages/user/user.php
@@ -14,19 +14,42 @@ $user = getUser($user_id); // Execute the function that will find the user with 
 
 $id = $_SESSION['user']['id'];
 
-echo $id;
-print_r(areWeFriends($id, $user_id));
-//$friendOrNotFriend = areWeFriends($id, $user_id);
-//echo $friendOrNotFriend;
+$friendOrNotFriend = areWeFriends($id, $user_id);
+
+?>
+
+<article>
+    <div class="container p-5 mt-5">
+
+<?php
+if ($friendOrNotFriend):
+    if ($friendOrNotFriend['status'] !== 1) :
+        ?>
+        <h2> <?php echo $user['first_name'] ?> </h2>
+        <h4>Member since <?php echo $user['created_at'] ?> </h4>
+        <a href="delete-friend.php?id=<?php echo $user['id']; ?>" class="btn btn-danger">UnFollow</a>
+    <?php
+    else :
+        ?>
+        <h2> <?php echo $user['first_name'] ?> </h2>
+        <h4>Member since <?php echo $user['created_at'] ?> </h4>
+        <a href="add-friend.php?id=<?php echo $user['id']; ?>" class="btn btn-primary">Follow</a>
+    <?php
+    endif;
+
+else:
+    ?>
+    <h2> <?php echo $user['first_name'] ?> </h2>
+    <h4>Member since <?php echo $user['created_at'] ?> </h4>
+    <a href="add-friend.php?id=<?php echo $user['id']; ?>" class="btn btn-primary">Follow</a>
+        <?php
+endif;
 
 ?>
 
 
-<article>
-    <div class="container p-5 mt-5">
-        <h2> <?php echo $user['first_name'] ?> </h2>
-        <h4>Member since <?php echo $user['created_at'] ?> </h4>
-        <a href="add-friend.php?id=<?php echo $user['id']; ?>" class="btn btn-primary">Follow</a>
+
+
     </div>
 </article>
 
